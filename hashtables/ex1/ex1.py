097a83b072a8f705d31379b472b1d6b9f9d38d99
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-09-12 13:21:22 -0400

    ex 1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,25 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+   # Establishing dictionary for values
+    the_cache = {}
+    sm_index = 0
+    lrg_index = 0
+    # Looping through list and put the values in the dictionary
+    for i in range(len(weights)):
+
+        if weights[i] not in the_cache:
+            the_cache[weights[i]] = i
+            if limit - weights[i] == weights[i]:
+                continue
+        if limit - weights[i] in the_cache:
+            # finding out what is the smaller of the two values min = a if a < b else b
+            if i > the_cache[limit - weights[i]]:
+                sm_index = the_cache[limit - weights[i]]
+                lrg_index = i
+            else:
+                sm_index = i
+                lrg_index = the_cache[limit - weights[i]]
+            return (lrg_index, sm_index)
 
     return None
